2f176e04b46d05d3ae043e6261381b5eccb15d72
Zhangxz010 <147686443+Zhangxz010@users.noreply.github.com>
2025-04-08 09:40:38 +0200

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -21,7 +21,6 @@ function Home() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
 
         <div className="input-container">
           <input
